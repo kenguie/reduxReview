9ffff2a3d525bb5aec6c75e099b85f78dc9a08b6
Kenrick Guie <nyhunter77@yahoo.com>
2018-02-26 16:52:47 -0500

    testing that the action triggers the reducer

--- src/index.js
+++ src/index.js
@@ -6,12 +6,12 @@ import { createStore } from 'redux';
 import { Provider } from 'react-redux';
 
 import rootReducer from './reducers';
-// import { addCharacterById } from './actions';
+import { addCharacterById } from './actions';
 
 const store = createStore(rootReducer);
 console.log('store.getState()', store.getState());
-// store.subscribe(() => console.log('store', store.getState()));
-// store.dispatch(addCharacterById(2));
+store.subscribe(() => console.log('store', store.getState()));
+store.dispatch(addCharacterById(2));
 
 ReactDOM.render(
   <Provider store={store}>
